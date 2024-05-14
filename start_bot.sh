b8122c2d120faada19b8bd4017fc4a0aefdbc5de
Norbert Takacs <norbert.takacs@postnord.com>
2024-05-14 15:11:37 +0200

    doubled heap

--- start_bot.sh
+++ start_bot.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 PYTHON_LEGACY_DATABASE=config/rss.db
 PYTHON_LEGACY_DATABASE_BACKUP=config/backup_python_rss.db
 
@@ -10,18 +9,17 @@ PRISMA=./node_modules/.bin/prisma
 
 if test -f "$PYTHON_LEGACY_DATABASE"; then
     echo "INFO: Old database exists:$PYTHON_LEGACY_DATABASE, backing it up as backup_python_rss.db"
-    
+
     cp $PYTHON_LEGACY_DATABASE $PYTHON_LEGACY_DATABASE_BACKUP
-    
-    sqlite3 $PYTHON_LEGACY_DATABASE < prisma/migrations/python_to_js_migration.sql
-    
+
+    sqlite3 $PYTHON_LEGACY_DATABASE <prisma/migrations/python_to_js_migration.sql
+
     mv $PYTHON_LEGACY_DATABASE $JS_DATABASE
-    
+
     $PRISMA migrate resolve --applied init
     $PRISMA migrate depoy
 fi
 
-
 $PRISMA migrate deploy
 
-node --max_old_space_size=1024 dist/src/main.js
+node --max_old_space_size=2048 dist/src/main.js
